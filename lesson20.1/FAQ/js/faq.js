87be18eda2445e94ef4ae3df57a198d5de193403
NItram228 <martin.kluka.m@gmail.com>
2022-01-14 17:53:52 +0300

    lesson20.1-2.3

--- lesson20.1/FAQ/js/faq.js
+++ lesson20.1/FAQ/js/faq.js
@@ -2,10 +2,13 @@ $(document).ready(function() {
     $('.block-title').click(function(event){
         $(this).toggleClass('active').next().slideToggle(300);
     })  
-    $('.number').click(function(event){
+    $('.block-item').click(function(event){
         $(this).toggleClass('active').next().slideToggle(300);
     })
     $('.block-social-icon').click(function(event){
+        $(this).toggleClass('active').next().slideToggle(300);
+    })
+    $('.number').click(function(event){
         $(this).toggleClass('active').next().slideToggle(300);
     })
 })
