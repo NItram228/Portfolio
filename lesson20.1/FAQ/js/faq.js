8a82b40c08b92ee3cc0c1cf2e81eb7363893923e
NItram228 <martin.kluka.m@gmail.com>
2022-01-14 16:19:33 +0300

    lesson20.1-2.2

--- lesson20.1/FAQ/js/faq.js
+++ lesson20.1/FAQ/js/faq.js
@@ -5,4 +5,7 @@ $(document).ready(function() {
     $('.number').click(function(event){
         $(this).toggleClass('active').next().slideToggle(300);
     })
+    $('.block-social-icon').click(function(event){
+        $(this).toggleClass('active').next().slideToggle(300);
+    })
 })
