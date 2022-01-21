57b561290e6c4c5b89a0a064c2a58f518476db12
NItram228 <martin.kluka.m@gmail.com>
2022-01-21 17:59:33 +0300

    lesson22

---
+++ lesson22/script.js
@@ -0,0 +1,51 @@
+// 1
+let topSpeed = 160;
+let distance = 617.54;
+let login = "mango935";
+let isOnline =  true;
+let isAdmin = false;
+console.log(topSpeed);
+console.log(distance);
+console.log(login);
+console.log(isOnline);
+console.log(isAdmin);
+
+// 2
+
+console.log(topSpeed , distance , login ,isOnline , isAdmin);
+
+// 3
+
+let nema = 'Hi';
+let isNema = 'Сергей !';
+
+console.log(nema + " " + isNema)
+
+//4
+
+let a = 10;
+let b = 2;
+
+let sum = a + b;
+let otrez = a - b; 
+let multiplying = a * b;
+let delenia = a / b;
+
+console.log(sum);
+let messageNew = `Сума, ${sum}`;
+console.log(otrez);
+console.log(multiplying);
+console.log(delenia);
+console.log(messageNew)
+
+//5
+
+let pricePerDroid = 800;
+let orderedQuantily = 6;
+let deliveryFee = 50;
+
+let sumTwo = pricePerDroid * orderedQuantily + deliveryFee;
+
+let price =  `цена: ${pricePerDroid}, количество: ${orderedQuantily}, стоимость доставки: ${deliveryFee}, общая стоимость: ${sumTwo}`;
+
+console.log(price);
