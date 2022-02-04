b0de06c7aeded8c79df9e3744138e8a81a8629fd
NItram228 <martin.kluka.m@gmail.com>
2022-02-04 18:26:15 +0300

    lesson24

--- lesson24/script.js
+++ lesson24/script.js
@@ -1,3 +1,4 @@
+/*
 //1
 let number_1 = 9;
 let number_2 = 6;
@@ -73,7 +74,7 @@ let ADMIN_PASSWORD = ('js')
 if(password === ADMIN_PASSWORD){
     console.log('Welcome!')
 }
-else if(password === null){
+else if(password == 0){
     console.log('Canceled by user!')
 }
 else{
@@ -115,3 +116,94 @@ let taext = 'message'
 let taext_2 = taext.toUpperCase().slice(0,1)
 
 console.log(taext_2)
+
+
+*/
+//11
+let country = ['China ', 'Chile ', 'Australia ', 'Jamaica ']
+let price = ['100', '250', '170' , '120']
+// let country_name = 'Jamaica'
+China
+Chile 
+Australia 
+Jamaica 
+
+
+
+
+function countryFunction(){
+    const country_name = prompt ('write your country') 
+    
+    switch (country_name){
+        case 'China':
+            console.log(country[0] + '= ' + price[0])
+        break;
+        case 'Chile':
+            console.log(country[1] + '= ' + price[1])
+        break;
+        case 'Australia':
+            console.log(country[2] + '= ' + price[2])
+        break;
+        case 'Jamaica':
+            console.log(country[3] + '= ' + price[3])
+        break;
+        default:
+            console.log(':(')
+    }
+    
+    return
+}
+
+
+//12
+
+const age = function(){
+
+    const youAge = prompt('write your age')
+
+    if ( youAge >= 18){
+        console.log('You are an aduit')
+    }
+    else{
+        console.log('You are a minor')
+    }
+    return
+}
+age();
+
+//13
+
+
+const passwordFunction = function(){
+
+    const password = prompt('write your password')
+
+    let ADMIN_PASSWORD = ('jqueryismyjam')
+    
+    if(password === ADMIN_PASSWORD){
+        console.log('Welcome!')
+    }
+    else{
+        console.log('Access deniad, wrong password')
+    }
+    return
+}
+
+passwordFunction();
+
+//14
+let available = 100
+function checkStorage(){
+
+    const ordered = prompt('количество товара в заказе')
+
+    if(ordered < available){
+        console.log('The order is accepted, our manager will contact you')
+    }
+    else{
+        console.log('Your order is too large, not enough goods in stock!')
+    }
+    return
+} 
+
+checkStorage()
