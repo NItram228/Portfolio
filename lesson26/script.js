c5a8bd1141d6926fe4a4d4ac0168d1a469f35994
NItram228 <martin.kluka.m@gmail.com>
2022-02-18 18:06:36 +0300

    lesson26

--- lesson26/script.js
+++ lesson26/script.js
@@ -1,4 +1,4 @@
-const colors = ['red', 'pink', 'violet', 'green', 'tomato', 'teal'];
+/*const colors = ['red', 'pink', 'violet', 'green', 'tomato', 'teal'];
 const list = document.querySelector('.link');
 console.log(list)
 console.log(colors);
@@ -13,4 +13,24 @@ for (let el of colors) {
 }
 console.log(links);
 
-list.append(...links);
+list.append(...links);*/
+const array = [3, 5, 7, 11, 33, 2, 6];
+
+function getSum(array, callblack){
+    let total = 0
+    for (let el of array){
+    
+        if(callblack(el)){
+            total +=el;
+        } 
+    }
+
+    return total;
+}
+
+const isGrearterFore = el => el >4;
+const isGrearOdd = el => el % 2 !== 0 
+const isGrearEven = el => el % 2 === 0;
+console.log('return 4:', getSum(array, isGrearterFore))
+console.log('return 1:', getSum(array, isGrearOdd))
+console.log('return 2:', getSum(array, isGrearEven))
